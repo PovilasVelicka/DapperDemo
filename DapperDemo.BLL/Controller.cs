9db38667c237b38ed9a8e564a683d4ab5211483d
Povilas Velicka <velicka.povilas@gmail.com>
2022-06-28 21:33:49 +0300

    Add project files.

---
+++ DapperDemo.BLL/Controller.cs
@@ -0,0 +1,71 @@
+﻿using DapperDemo.DAL;
+using System.Data;
+
+namespace DapperDemo.BLL
+{
+    public class Controller: IDisposable
+    {
+        const string SELECT_ALL_BOOKS = "SELECT * FROM dbo.Books ";
+        const string WHERE_BOOKS_BY_NAME = "WHERE Name = @book_name";
+        const string BOOKS_PROCEDURE = "dbo.getBooksWithPages";
+
+        const string SELECT_ALL_CATEGORIES = "SELECT * FROM dbo.Categories ";
+        const string WHERE_CATEGORIES_BY_NAME = "WHERE Name = @category_name";
+
+        private readonly DbRepository _repository;
+
+        public Controller()
+        {
+            _repository = new DbRepository();
+        }
+
+        public List<Book> GetBooks(string bookName)
+        {
+            return _repository.Get<Book>(
+                $"{SELECT_ALL_BOOKS}{WHERE_BOOKS_BY_NAME}",
+                new { book_name = bookName });
+        }
+
+        public List<Book> GetBooks()
+        {
+            return _repository.Get<Book>($"{SELECT_ALL_BOOKS}");
+        }
+
+        public List<Book> GetBooksFromProc()
+        {
+            return _repository.Get<Book>(BOOKS_PROCEDURE, commandType: CommandType.StoredProcedure);
+        }
+
+        public List<Categories> GetCategories(string categoryName)
+        {
+            return _repository.Get<Categories>(
+               $"{SELECT_ALL_CATEGORIES}{WHERE_CATEGORIES_BY_NAME}",
+               new { category_name = categoryName });
+        }
+
+        public List<Categories> GetCategories()
+        {
+            return _repository.Get<Categories>($"{SELECT_ALL_CATEGORIES}");
+        }
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _repository.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+
+        }
+    }
+}
---
+++ DapperDemo/Program.cs
@@ -0,0 +1,62 @@
+﻿using DapperDemo.BLL;
+
+using (var _controller = new Controller())
+{
+    var books = _controller.GetBooks("Harry Potter");
+
+    var books2 = _controller.GetBooksFromProc();
+
+    var categories = _controller.GetCategories("Fantasy");
+}
+
+
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using var connection = new SqlConnection(@"Server=localhost\SQLEXPRESS;Database=BooksDbManyToMany;Trusted_Connection=True;");
+//connection.Open();
+//var sql = $"SELECT * FROM dbo.Books WHERE Name = @book_name";
+//var sqlParams = new { book_name = "Harry Potter" };
+//var books = connection.Query<Book>(sql, sqlParams);
+//Console.WriteLine();
+
+//string jsonString = JsonSerializer.Serialize(books);
+//sqlParams = new { book_name = jsonString };
+
+//var resutl = connection.Query("dbo.getBooksWithPages",  commandType: System.Data.CommandType.StoredProcedure);
+//Console.WriteLine();
